3c233c115b372ba3372d2c889470eb25ad4e64b8
Laveezah Noor <64427229+laveezah-noor@users.noreply.github.com>
2020-06-27 23:46:08 +0500

    files added

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function getCalculation(){
+    var calculation = 
+}
+
+function getNumber(num){
+    var result = document.getElementById("result");
+    result.value += num;
+}
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = ""
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    console.log(result.value);
+    result.value = eval(result.value)
+}
+
+function backspace(){
+    var result = document.getElementById("result");
+    back =  result.value;
+    result.value = back.slice(0,-1)
+}
